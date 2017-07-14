228d81b25f668fe2cbb91bbed66b55541445b58e
rickydazla <rick@tricky3.co.uk>
2017-07-13 23:54:30 -0700

    install gulp, updates ignore

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var pump = require('pump');
+var tinify = require('gulp-tinify');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var del = require('del');
+
+// identify Shopify assets
+var assets = './theme/assets/'
+var jsScripts = [assets + 'tricky3.*js', '!' + assets + 'tricky3*.min.js'];
+
+// minify scripts
+gulp.task('scripts', function(){
+  pump([
+    gulp.src(jsScripts),
+    rename(function(script) {
+      script.extname = '.min.js';
+      return script;
+    }),
+    uglify(),
+    gulp.dest(assets)
+  ])
+});
+
+// Watch
+
+gulp.task('watch-scripts', function(){
+  var tasks = ['scripts'];
+  gulp.watch([jsScripts], tasks);
+});
+
+gulp.task('default', ['watch-scripts']);
